b0bb155c1f57ed88e87f23fef717346b2925ab14
pmiks <e.pmiks@yandex.ru>
2020-11-04 12:54:49 +0400

    5

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function index(){
         router.push(`/payform/${id}`)
         console.log(id)
     }
-    return <><div className={s.mobileListHeader}>Выберите оператора:</div>
+    return <AppForm><div className={s.mobileListHeader}>Выберите оператора:</div>
         {mobileOperatorList.map(item=><MobileOperator item={item} onClick={selectMobileOperator}/>)}
-    </>
+    </AppForm>
 }
